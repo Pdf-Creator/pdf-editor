65843882531e5f4871f09e6f30109acf5ffb005c
alexbuyan <sashabuyan@yandex.ru>
2022-03-15 19:57:13 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module hse.btf.pdfeditor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens hse.btf.pdfeditor to javafx.fxml;
+    exports hse.btf.pdfeditor;
+}
