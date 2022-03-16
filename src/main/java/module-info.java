5ae11292675de16af3435afdb0455312434330ee
alexbuyan <sashabuyan@yandex.ru>
2022-03-16 12:27:48 +0300

    initializing branch with installing needed libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,14 @@ module hse.btf.pdfeditor {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.swing;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+
+    requires jlatexmath;
 
     opens hse.btf.pdfeditor to javafx.fxml;
     exports hse.btf.pdfeditor;
