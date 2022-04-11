575ad35a952f495b8534dd4f3680550a448d4c4a
TurovV <vladimir.turov.2002@gmail.com>
2022-04-11 11:02:20 +0300

    basic interface

--- src/main/java/hse/btf/pdfeditor/PdfEditorController.java
+++ src/main/java/hse/btf/pdfeditor/PdfEditorController.java
@@ -1,14 +1,30 @@
 package hse.btf.pdfeditor;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PdfEditorController {
     @FXML
-    private Label welcomeText;
+    private Button createButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void createProjectButton(ActionEvent event) throws IOException {
+        Stage thisStage = (Stage)createButton.getScene().getWindow();
+        thisStage.close();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("work-window.fxml"));
+
+        var stage = new Stage();
+        stage.setTitle("Working Window");
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.show();
     }
+
 }
