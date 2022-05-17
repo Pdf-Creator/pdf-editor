364cf95a91b0153f0ebdd45fa6e42750df898e65
diffitask <ginger.ll@mail.ru>
2022-05-17 13:34:27 +0300

    updated ListView in HelloScene

--- src/main/java/hse/btf/pdfeditor/HelloSceneController.java
+++ src/main/java/hse/btf/pdfeditor/HelloSceneController.java
@@ -1,19 +1,50 @@
 package hse.btf.pdfeditor;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class HelloSceneController {
+public class HelloSceneController implements Initializable {
+    @FXML
+    private TextField projectNameField;
+    // добавить выгрузку имен проектов из файла (десериализация) -- при создании сцены
+
     @FXML
     private Button createButton;
+
+    @FXML
+    private ListView<String> projectNamesListView;
+
+    private List<String> projectNamesList = new ArrayList<>();
+    private String currentProjectName = "";
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        projectNamesListView.getItems().addAll(projectNamesList);
+        projectNamesListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                currentProjectName = projectNamesListView.getSelectionModel().getSelectedItem();
+                projectNameField.setText(currentProjectName);
+            }
+        });
+    }
 
     @FXML
     void createProjectButton(ActionEvent event) throws IOException {
