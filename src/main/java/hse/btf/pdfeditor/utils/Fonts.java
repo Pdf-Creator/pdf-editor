9e433ebc58b6d626e8263275304b0072c292b9a9
alexbuyan <sashabuyan@yandex.ru>
2022-06-25 19:37:04 +0300

    adding fonts

---
+++ src/main/java/hse/btf/pdfeditor/utils/Fonts.java
@@ -0,0 +1,11 @@
+package hse.btf.pdfeditor.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class Fonts {
+    public static final @NotNull String FONTS_DIR = "src/main/resources/fonts/";
+
+    public static final @NotNull String FREE_SANS = FONTS_DIR + "freesans.ttf";
+    public static final @NotNull String FREE_SANS_BOLD = FONTS_DIR + "freesansbold.ttf";
+    public static final @NotNull String ARIAL = FONTS_DIR + "arial.ttf";
+}
